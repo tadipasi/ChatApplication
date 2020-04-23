2c6ac42ad4a8bed0b02849d4128b6c3b369cbcf2
Tadiwa Pasipanodya <tadipasi@gmail.com>
2020-04-23 08:13:55 -0600

    create the ejs for the homepage and chatrooms add the routes to get the homepage and chatrooms

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require("express");
 const app = express();
 const server = require('http').Server(app);
 
+app.set("view engine", ejs);
 
+app.get("/", function(req,res){
+    res.send("home page")
+});
+
+app.get("/chatrooms",function(req,res){
+    res.send
+});
 
 server.listen(8000, function(){
         console.log("Server is running")
