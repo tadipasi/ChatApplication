b804b2e6cf8a7efe37012490d9668a9c87ba22d9
Tadiwa Pasipanodya <tfp482@mail.usask.ca>
2020-05-15 15:10:46 -0600

    Add the model for the chatroom and make the connection to the mongoDB database using mongoose

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 const server = require('http').Server(app);
+const mongoose = require("mongoose");
 
-app.set("view engine", ejs);
+
+//Connect to database
+mongoose.connect('mongodb+srv://tadipasi:barcelona@cluster0-81zn9.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to database!');
+}).catch(err => {
+    console.log('ERROR:', err.message);
+});
+
+
+
+app.set("view engine", "ejs");
 
 app.get("/", function(req,res){
     res.send("home page")
